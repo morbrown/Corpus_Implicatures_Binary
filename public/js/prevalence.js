f2d53a1ef930a3ce8df3102f018ae4e5c113cb48
morbrown <morbrown.stanford.edu>
2023-05-25 17:11:25 -0700

    added turk code chunk back into prevalence.js

--- public/js/prevalence.js
+++ public/js/prevalence.js
@@ -532,14 +532,14 @@ function init() {
   $('.slide').hide(); //hide everything
 
   //make sure turkers have accepted HIT (or you're not in mturk)
-  /* $("#start_button").click(function() {
+   $("#start_button").click(function() {
     if (turk.previewMode) {
       $("#mustaccept").show();
     } else {
       $("#start_button").click(function() {$("#mustaccept").show();});
       exp.go();
     }
-  }); */
+  }); 
 
   exp.go(); //show first slide
 }
