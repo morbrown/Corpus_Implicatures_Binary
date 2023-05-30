ff3aba5525ba7dd79a8250da0ee92775e1ed1f5d
thegricean <judithdegen@gmail.com>
2023-05-29 18:25:36 -0700

    updated r script and reorganized

--- analysis/rscripts/figures_and_analysis_pilot.R
+++ analysis/rscripts/figures_and_analysis_pilot.R
@@ -2,23 +2,19 @@
 # code for generating the figures and analysis reported in 
 # Degen, J. (2015). Investigating the distribution of _some_ (but not _all_) implicatures using corpora and web-based methods. Semantics & Pragmatics.
 
-library(Hmisc)
-library(gridExtra)
-library(ggplot2)
-library(bootstrap)
-library(MuMIn)
-library(lme4)
-library(dplyr)
-library(tidyr)
 library(tidyverse)
+library(lme4)
+library(gridExtra)
+
 theme_set(theme_bw())
+cbPalette <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 
 # set your path here
-setwd("/Users/morbrown/code/corpus_implicatures_binary")
-source("Rscripts/helpers.R")
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+source("helpers.R")
 
 # read data
-raw_data = read.table("data_binary/corpus_implicatures_binary_pilot-trials.tsv",sep="\t",header=T,quote="")
+raw_data = read_tsv("../data/corpus_implicatures_binary_pilot-trials.tsv",quote="")
 
 raw_data_updated = raw_data %>%
   filter(!tgrep_id=="bot_check") %>%
@@ -30,70 +26,160 @@ class(raw_data_updated$means_same)
 raw_data_updated$means_same = as.numeric(as.character(raw_data_updated$means_same)) 
 
 class(raw_data_updated$means_same)
+summary(raw_data_updated)
 
 #Reading in data from original likert scale experiment, in which each item is 
 #coded as partitive or non-partitive. Then isolating only the tgrep id and 
 #partitive info for each item, and only keeping those items which appeared
 #in the pilot experiment
 
-judith_data = read.table("data_binary/some_database_copy.csv", sep="\t",header=T,quote="")
+judith_data = read_tsv("../data/some_database_copy.csv",quote="") %>% 
+  rename(tgrep_id=Item) %>% 
+  select(tgrep_id,Partitive,StrengthSome,Mention,Subjecthood,Modification,SentenceLength) %>% 
+  distinct() %>% 
+  mutate(Partitive=fct_recode(Partitive,"partitive"="yes","non-partitive"="no"),
+         Mention=fct_recode(Mention,"mediated"="med")) %>% 
+  mutate(Mention=fct_relevel(Mention,"new"))
+nrow(judith_data)
+view(judith_data)
 
-judith_data_partitive = judith_data %>%
-  select(Item, Partitive) %>%
-  distinct() %>%
-  mutate(tgrep_id = Item) %>%
-  select(tgrep_id, Partitive) %>%
-  filter(tgrep_id %in% raw_data_updated$tgrep_id)
+#Merging the case information into the binary judgment dataset
+d = raw_data_updated %>% 
+  left_join(judith_data, by = "tgrep_id")
+view(d)
 
-#Merging the partitive data with my data
+# plot histogram of by-item proportions
+props = d %>% 
+  group_by(tgrep_id) %>% 
+  summarize(Proportion = mean(means_same))
 
-data_partitive = merge(raw_data_updated, judith_data_partitive, by = "tgrep_id")
-
-agr = aggregate(means_same ~ tgrep_id, data=raw_data_updated, FUN=mean)
-
-ggplot(agr,aes(x=means_same)) +
+ggplot(props,aes(x=Proportion)) +
   geom_histogram() +
-  scale_x_continuous(name="Mean by-item implicature strength rating",breaks=seq(0,1,by=0.2)) 
-
-# Figure 10: Distribution of simulated mean by-item ratings.
-likert = c(1,2,3,4,5,6,7)
-means = data.frame(Mean=replicate(1363,mean(sample(likert,size=10,replace=T))))
-summary(means$Mean)
-sd(means$Mean)
-ggplot(means,aes(x=Mean)) +
-  geom_histogram() +
-  scale_y_continuous(limits=c(0,180)) +  
-  scale_x_continuous(limits=c(1,7),breaks=seq(1,7,by=1),name=c("Mean by-item rating (simulated)"))
-
+  ylab("Cases") +
+  scale_x_continuous(name="By-item implicature rate",breaks=seq(0,1,by=0.1)) 
+ggsave("../graphs/props_byitem.pdf",width=4,height=3)
 
 # Figure 2: Mean implicature strength ratings (left) and distribution of mean by-item ratings (right) for non-partitive and partitive some-NPs.
-agr_partitive = aggregate(means_same ~ tgrep_id + Partitive, data=data_partitive,FUN=mean)
-agrr_partitive = aggregate(means_same ~ Partitive, data=agr_partitive, FUN=mean)
-agrr_partitive$CILow = aggregate(means_same ~ Partitive, data=agr_partitive, FUN=ci.low)$means_same
-agrr_partitive$CIHigh = aggregate(means_same ~ Partitive, data=agr_partitive, FUN=ci.high)$means_same
-agrr_partitive$YMin = agrr_partitive$means_same - agrr_partitive$CILow
-agrr_partitive$YMax = agrr_partitive$means_same + agrr_partitive$CIHigh
-agrr_partitive$Freq = as.data.frame(table(agr_partitive$Partitive))$Freq
+agr = d %>% 
+  group_by(Partitive) %>% 
+  summarize(Proportion = mean(means_same), CILow=ci.low(means_same), CIHigh=ci.high(means_same)) %>% 
+  ungroup() %>% 
+  mutate(YMin=Proportion-CILow,YMax=Proportion+CIHigh)
 
-pm = ggplot(agrr_partitive,aes(x=Partitive,y=means_same,fill=Partitive)) +
+agr_id = d %>% 
+  group_by(tgrep_id, Partitive) %>% 
+  summarize(Proportion = mean(means_same)) 
+
+p_bar = ggplot(agr,aes(x=Partitive,y=Proportion,fill=Partitive)) +
   geom_bar(stat="identity",color="black",width=.5,show_guide=F) +
-  geom_errorbar(aes(ymin=0,ymax=1),width=.2) +
-  scale_x_discrete(name="",breaks=levels(agrr_partitive$Partitive),labels=c("non-partitive","partitive")) +
-  scale_fill_grey(start=0.7,end=0.35,name="") +  
-  scale_y_continuous("Mean implicature strength") +
-  geom_text(y=0.5,aes(label=Freq),size=4)
+  geom_errorbar(aes(ymin=YMin,ymax=YMax),width=.2) +
+  scale_fill_manual(values=cbPalette) +
+  scale_y_continuous("Mean implicature rate",limits=c(0,1)) 
 
-pm
+p_hist = ggplot(agr_id, aes(x=Proportion,fill=Partitive)) +
+  geom_histogram(alpha=.5,position="identity") +
+  ylab("Cases") +
+  coord_flip() +
+  scale_fill_manual(values=cbPalette) +
+  theme(axis.text.y=element_blank(),axis.title.y=element_blank(),axis.ticks.y=element_blank())
+p_hist
 
-agr = aggregate(Rating ~ Item + Partitive, data=d, FUN=mean)
-pd = ggplot(agr,aes(x=Rating,fill=Partitive)) +
-  geom_histogram(position="dodge") +
-  scale_fill_grey(start=0.7,end=0.35,breaks=levels(agr$Partitive),labels=c("non-partitive","partitive")) +
-  scale_y_continuous(name="Number of cases") +
-  scale_x_continuous("Mean by-item implicature strength",breaks=seq(1,7,by=1))
-pd
+pdf(file="../graphs/props_partitive.pdf",width=5,height=3)
+grid.arrange(p_bar,p_hist,nrow=1)
+dev.off()
 
-grid.arrange(pm,pd,nrow=1)
+# Figure 6: Mean implicature strength ratings (left) and distribution of mean by-item ratings (right) for other and subject some-NPs.
+agr = d %>% 
+  group_by(Subjecthood) %>% 
+  summarize(Proportion = mean(means_same), CILow=ci.low(means_same), CIHigh=ci.high(means_same)) %>% 
+  ungroup() %>% 
+  mutate(YMin=Proportion-CILow,YMax=Proportion+CIHigh)
+
+agr_id = d %>% 
+  group_by(tgrep_id, Subjecthood) %>% 
+  summarize(Proportion = mean(means_same)) 
+
+p_bar = ggplot(agr,aes(x=Subjecthood,y=Proportion,fill=Subjecthood)) +
+  geom_bar(stat="identity",color="black",width=.5,show_guide=F) +
+  geom_errorbar(aes(ymin=YMin,ymax=YMax),width=.2) +
+  scale_fill_manual(values=cbPalette) +
+  scale_y_continuous("Mean implicature rate",limits=c(0,1)) 
+
+p_hist = ggplot(agr_id, aes(x=Proportion,fill=Subjecthood)) +
+  geom_histogram(alpha=.5,position="identity") +
+  ylab("Cases") +
+  coord_flip() +
+  scale_fill_manual(values=cbPalette) +
+  theme(axis.text.y=element_blank(),axis.title.y=element_blank(),axis.ticks.y=element_blank())
+p_hist
+
+pdf(file="../graphs/props_subjecthood.pdf",width=5,height=3)
+grid.arrange(p_bar,p_hist,nrow=1)
+dev.off()
+
+
+# Figure 5: Mean implicature strength ratings (left) and distribution of mean by-item ratings (right) for new, mediated, and old embedded NP referents.
+agr = d %>% 
+  group_by(Mention) %>% 
+  summarize(Proportion = mean(means_same), CILow=ci.low(means_same), CIHigh=ci.high(means_same)) %>% 
+  ungroup() %>% 
+  mutate(YMin=Proportion-CILow,YMax=Proportion+CIHigh)
+
+agr_id = d %>% 
+  group_by(tgrep_id, Mention) %>% 
+  summarize(Proportion = mean(means_same)) 
+
+p_bar = ggplot(agr,aes(x=Mention,y=Proportion,fill=Mention)) +
+  geom_bar(stat="identity",color="black",width=.5,show_guide=F) +
+  geom_errorbar(aes(ymin=YMin,ymax=YMax),width=.2) +
+  scale_fill_manual(values=cbPalette) +
+  scale_y_continuous("Mean implicature rate",limits=c(0,1))
+
+p_hist = ggplot(agr_id, aes(x=Proportion,fill=Mention)) +
+  geom_histogram(alpha=.5,position="identity") +
+  ylab("Cases") +
+  coord_flip() +
+  scale_fill_manual(values=cbPalette) +
+  theme(axis.text.y=element_blank(),axis.title.y=element_blank(),axis.ticks.y=element_blank())
+p_hist
+
+grid.arrange(p_bar,p_hist,nrow=1)
+
+pdf(file="../graphs/props_mention.pdf",width=5,height=3)
+grid.arrange(p_bar,p_hist,nrow=1)
+dev.off()
+
+# Figure 7: Mean implicature strength ratings (left) and distribution of mean by-item ratings (right) for modified and unmodified some-NPs.
+agr = d %>% 
+  group_by(Modification) %>% 
+  summarize(Proportion = mean(means_same), CILow=ci.low(means_same), CIHigh=ci.high(means_same)) %>% 
+  ungroup() %>% 
+  mutate(YMin=Proportion-CILow,YMax=Proportion+CIHigh)
+
+agr_id = d %>% 
+  group_by(tgrep_id, Modification) %>% 
+  summarize(Proportion = mean(means_same)) 
+
+p_bar = ggplot(agr,aes(x=Modification,y=Proportion,fill=Modification)) +
+  geom_bar(stat="identity",color="black",width=.5,show_guide=F) +
+  geom_errorbar(aes(ymin=YMin,ymax=YMax),width=.2) +
+  scale_fill_manual(values=cbPalette) +
+  scale_y_continuous("Mean implicature rate",limits=c(0,1))
+
+
+p_hist = ggplot(agr_id, aes(x=Proportion,fill=Modification)) +
+  geom_histogram(alpha=.5,position="identity") +
+  ylab("Cases") +
+  coord_flip() +
+  scale_fill_manual(values=cbPalette) +
+  theme(axis.text.y=element_blank(),axis.title.y=element_blank(),axis.ticks.y=element_blank())
+p_hist
+
+pdf(file="../graphs/props_modification.pdf",width=5,height=3)
+grid.arrange(p_bar,p_hist,nrow=1)
+dev.off()
+
+# JD UPDATED FILE UP TO THIS POINT
 
 
 # Figure 3: Distribution of mean by-item determiner strength ratings overall (left) and conditioned on whether or not the some-NP was overtly partitive (right). Higher ratings indicate weaker determiner uses.
@@ -123,103 +209,7 @@ ggplot(agr,aes(x=StrengthSome,y=Rating)) +
   geom_smooth(method="lm",color="black",size=1)  
 
 
-# Figure 5: Mean implicature strength ratings (left) and distribution of mean by-item ratings (right) for new, mediated, and old embedded NP referents.
-agr = aggregate(Rating ~ Item + Mention, data=d, FUN=mean)
-agrr = aggregate(Rating ~ Mention, data=agr, FUN=mean)
-agrr$CILow = aggregate(Rating ~ Mention, data=agr, FUN=ci.low)$Rating
-agrr$CIHigh = aggregate(Rating ~ Mention, data=agr, FUN=ci.high)$Rating
-agrr$YMin = agrr$Rating - agrr$CILow
-agrr$YMax = agrr$Rating + agrr$CIHigh
-agrr$Freq = as.data.frame(table(agr$Mention))$Freq
-agrr$Mention = factor(x=as.character(agrr$Mention),levels=c("new","med","old"))
 
-pm = ggplot(agrr,aes(x=Mention,y=Rating, fill=Mention)) +
-  geom_bar(stat="identity",color="black",width=.5,show_guide=F) +
-  geom_errorbar(aes(ymin=YMin,ymax=YMax),width=.2) +
-  scale_fill_manual(values=c("gray60","gray85","gray30")) +  
-  scale_x_discrete(name="",breaks=levels(agrr$Mention),labels=c("new","mediated","old")) +
-  scale_y_continuous("Mean implicature strength") +
-  geom_text(y=0.5,aes(label=Freq),size=4)
-pm
-
-agr = aggregate(Rating ~ Item + Mention, data=d, FUN=mean)
-agr$Mention = factor(x=as.character(agr$Mention),levels=c("new","med","old"))
-pd = ggplot(agr,aes(x=Rating,fill=Mention)) +
-  geom_histogram(position="dodge") +
-  scale_fill_manual(values=c("gray60","gray85","gray30")) +
-  scale_y_continuous(name="Number of cases") +
-  scale_x_continuous("Mean by-item implicature strength",breaks=seq(1,7,by=1))
-pd
-
-grid.arrange(pm,pd,nrow=1)
-
-
-# Figure 6: Mean implicature strength ratings (left) and distribution of mean by-item ratings (right) for other and subject some-NPs.
-
-judith_data_subjecthood = judith_data %>%
-  select(Item, Subjecthood) %>%
-  distinct() %>%
-  mutate(tgrep_id = Item) %>%
-  select(tgrep_id, Subjecthood) %>%
-  filter(tgrep_id %in% raw_data_updated$tgrep_id)
-
-data_subjecthood = merge(raw_data_updated, judith_data_subjecthood, by = "tgrep_id")
-
-agr_subj = aggregate(means_same ~ tgrep_id + Subjecthood, data=data_subjecthood, FUN=mean)
-agrr_subj = aggregate(data=agr_subj, means_same ~ Subjecthood, FUN=mean)
-agrr_subj$CILow = aggregate(means_same ~ Subjecthood, data=agr_subj, FUN=ci.low)$means_same
-agrr_subj$CIHigh = aggregate(means_same ~ Subjecthood, data=agr_subj, FUN=ci.high)$means_same
-agrr_subj$YMin = agrr_subj$means_same - agrr_subj$CILow
-agrr_subj$YMax = agrr_subj$means_same + agrr_subj$CIHigh
-agrr_subj$Freq = as.data.frame(table(agr_subj$Subjecthood))$Freq
-
-pm_subj = ggplot(agrr_subj,aes(x=Subjecthood,y=means_same,fill=Subjecthood)) +
-  geom_bar(stat="identity",color="black",width=.5,show_guide=F) +
-  geom_errorbar(aes(ymin=YMin,ymax=YMax),width=.2) +
-  scale_fill_grey(start=.9,end=.35) +
-  scale_x_discrete(name="",breaks=levels(agrr_subj$Subjecthood),labels=c("other","subject")) +
-  scale_y_continuous("Mean implicature strength") +
-  geom_text(y=0.5,aes(label=Freq),size=4)
-pm_subj
-
-agr = aggregate(Rating ~ Item + Subjecthood, data=d, FUN=mean)
-pd = ggplot(agr,aes(x=Rating,fill=Subjecthood)) +
-  geom_histogram(position="dodge") +
-  scale_fill_grey(start=0.7,end=0.35,breaks=levels(agr$Subjecthood),labels=c("other","subject"),name="Subjecthood") +
-  scale_y_continuous(name="Number of cases") +
-  scale_x_continuous("Mean by-item implicature strength",breaks=seq(1,7,by=1))
-pd
-
-grid.arrange(pm,pd,nrow=1)
-
-
-# Figure 7: Mean implicature strength ratings (left) and distribution of mean by-item ratings (right) for modified and unmodified some-NPs.
-agr = aggregate(Rating ~ Item + Modification, data=d, FUN=mean)
-agrr = aggregate(data=agr, Rating ~ Modification, FUN=mean)
-agrr$CILow = aggregate(Rating ~ Modification, data=agr, FUN=ci.low)$Rating
-agrr$CIHigh = aggregate(Rating ~ Modification, data=agr, FUN=ci.high)$Rating
-agrr$YMin = agrr$Rating - agrr$CILow
-agrr$YMax = agrr$Rating + agrr$CIHigh
-agrr$Freq = as.data.frame(table(agr$Modification))$Freq
-
-pm=ggplot(agrr,aes(x=Modification,y=Rating,fill=Modification)) +
-  geom_bar(stat="identity",color="black",width=.5,show_guide=F) +
-  geom_errorbar(aes(ymin=YMin,ymax=YMax),width=.2) +
-  scale_x_discrete(name="",breaks=levels(agrr$Modification),labels=c("modified","unmodified")) +
-  scale_fill_grey(start=.7,end=.25) +
-  scale_y_continuous("Mean implicature strength") +
-  geom_text(y=0.5,aes(label=Freq),size=4)
-pm
-
-agr = aggregate(Rating ~ Item + Modification, data=d, FUN=mean)
-pd = ggplot(agr,aes(x=Rating,fill=Modification)) +
-  geom_histogram(position="dodge") +
-  scale_fill_grey(start=0.7,end=0.25,breaks=levels(agr$Modification),labels=c("modified","unmodified"),name="Modification") +
-  scale_y_continuous(name="Number of cases") +
-  scale_x_continuous("Mean by-item implicature strength",breaks=seq(1,7,by=1))
-pd
-
-grid.arrange(pm,pd,nrow=1)
 
 # Figure 8: Mean implicature strength ratings by linguistic mention (old/new embedded NP referent), subjecthood (subject/other some-NP), and modification (modified/unmodified embedded NP).
 d$redMention = as.factor(ifelse(d$Mention == "new","new","old"))
